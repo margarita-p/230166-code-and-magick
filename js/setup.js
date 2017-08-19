4e6d9251bf86e870cd56be69a2829ecc6f365cb3
margarita-p <dayzzzy@gmail.com>
2017-08-19 17:37:09 +0300

    Третье задание 2

--- js/setup.js
+++ js/setup.js
@@ -6,14 +6,19 @@ var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 10
 var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+var friendsBlock = userDialog.querySelector('.setup-similar');
+
+var showDOMElement = function (elem) {
+  elem.classList.remove('hidden');
+};
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
 var getRandom = function (arr) {
   var rand = Math.floor(Math.random() * arr.length);
-  return arr[rand];
+  var removed = arr.splice(rand, 1);
+  return removed;
 };
 
 var wizards = [
@@ -47,12 +52,14 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var fragment = document.createDocumentFragment();
+var appendWizards = function (arr) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < arr.length; i++) {
+    fragment.appendChild(renderWizard(arr[i]));
+  }
+  similarListElement.appendChild(fragment);
+};
 
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
-
-similarListElement.appendChild(fragment);
-
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+appendWizards(wizards);
+showDOMElement(userDialog);
+showDOMElement(friendsBlock);
